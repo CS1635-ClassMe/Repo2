edc6eb6dfd49deee9a11f841b81d83122fafa147
professir <buckcyoung@gmail.com>
2014-03-25 15:47:17 -0400

    ROBERTS SO DUMB OMG LOL

--- ClassMe/src/com/cs1635/classme/SearchActivity.java
+++ ClassMe/src/com/cs1635/classme/SearchActivity.java
@@ -33,7 +33,7 @@ public class SearchActivity extends ActionBarActivity {
         result.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(activity, ClassView.class);
+                Intent intent = new Intent(activity, CourseStreamActivity.class);
                 startActivity(intent);
             }
         });
