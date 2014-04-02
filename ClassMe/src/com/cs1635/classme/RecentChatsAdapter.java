6e1a9642f3a38aef3bbc89d0333a892689a170d2
professir <buckcyoung@gmail.com>
2014-04-01 20:44:27 -0400

    Add Cal Layout files

--- ClassMe/src/com/cs1635/classme/RecentChatsAdapter.java
+++ ClassMe/src/com/cs1635/classme/RecentChatsAdapter.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shared.TextMessage;
@@ -48,7 +47,7 @@ public class RecentChatsAdapter extends ArrayAdapter<ArrayList<TextMessage>>
 		for(TextMessage message : recentChats.get(position))
 		{
 			if(!nameString.contains(message.getFrom()))
-				nameString += message.getFrom() + ",";
+				nameString += message.getFrom() + ", ";
 		}
 		if(nameString.endsWith(","))
 			nameString = nameString.substring(0,nameString.length()-1);
