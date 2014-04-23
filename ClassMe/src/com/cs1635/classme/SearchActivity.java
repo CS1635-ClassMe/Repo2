5ffa7ac0fe9f142e1299f9416a3ef91de96f8f37
Robert McDermot <robert.mcdermot@gmail.com>
2014-04-23 17:27:37 -0400

    commit before pull

--- ClassMe/src/com/cs1635/classme/SearchActivity.java
+++ ClassMe/src/com/cs1635/classme/SearchActivity.java
@@ -1,6 +1,5 @@
 package com.cs1635.classme;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -9,36 +8,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
-public class SearchActivity extends ActionBarActivity {
-    SearchActivity activity = this;
+public class SearchActivity extends ActionBarActivity
+{
+	SearchActivity activity = this;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.search);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
 
-        //find button set listener
-        Button submitSearch = (Button) findViewById(R.id.search_submit);
+		//find button set listener
+		Button submitSearch = (Button) findViewById(R.id.search_submit);
 
-        submitSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(activity, ResultsActivity.class);
-                startActivity(intent);
-            }
-        });
+		submitSearch.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
 
-        //call new intent in lister callback
-    }
+			}
+		});
+	}
 
+	public void onCheckboxClicked(View view)
+	{
+		// Is the view now checked?
+		boolean checked = ((CheckBox) view).isChecked();
 
-
-    public void onCheckboxClicked(View view){
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-
-        // Check which checkbox was clicked
-        /*switch(view.getId()) {
+		// Check which checkbox was clicked
+		/*switch(view.getId()) {
             case R.id.search_filter_activecourse:
                 if (checked)
                 // Put some meat on the sandwich
@@ -54,26 +53,29 @@ public class SearchActivity extends ActionBarActivity {
             // TODO: Veggie sandwich
         }*/
 
-    }
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.search, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.search, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if(id == R.id.action_settings)
+		{
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
--- ClassMe/src/com/shared/Course.java
+++ ClassMe/src/com/shared/Course.java
@@ -2,157 +2,38 @@ package com.shared;
 
 import java.io.Serializable;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class Course.
- */
+@SuppressWarnings("serial")
 public class Course implements Serializable
 {
-	
-	/** The Constant serialVersionUID. */
-	private static final long serialVersionUID = -6434443948276427528L;
-	
-	/** The course request key. */
-	private String subjectCode, courseName, courseDescription, courseRequestKey;
-	
-	/** The course number. */
-	private int courseNumber;
-	
-	/**
-	 * Instantiates a new course.
-	 */
-	public Course(){}
-	
-	/**
-	 * Instantiates a new course.
-	 *
-	 * @param code the code
-	 * @param num the num
-	 * @param name the name
-	 * @param desc the desc
-	 */
-	public Course(String code, int num, String name, String desc)
+	private String courseId, instructor, department;
+
+	public String getCourseId()
 	{
-		subjectCode = code;
-		courseNumber = num;
-		courseName = name;
-		courseDescription = desc;
+		return courseId;
 	}
 
-	/**
-	 * Gets the subject code.
-	 *
-	 * @return the subject code
-	 */
-	public String getSubjectCode()
+	public void setCourseId(String courseId)
 	{
-		return subjectCode;
+		this.courseId = courseId;
 	}
 
-	/**
-	 * Sets the subject code.
-	 *
-	 * @param subjectCode the new subject code
-	 */
-	public void setSubjectCode(String subjectCode)
+	public String getInstructor()
 	{
-		this.subjectCode = subjectCode;
+		return instructor;
 	}
 
-	/**
-	 * Gets the course name.
-	 *
-	 * @return the course name
-	 */
-	public String getCourseName()
+	public void setInstructor(String instructor)
 	{
-		return courseName;
+		this.instructor = instructor;
 	}
 
-	/**
-	 * Sets the course name.
-	 *
-	 * @param courseName the new course name
-	 */
-	public void setCourseName(String courseName)
+	public String getDepartment()
 	{
-		this.courseName = courseName;
+		return department;
 	}
 
-	/**
-	 * Gets the course description.
-	 *
-	 * @return the course description
-	 */
-	public String getCourseDescription()
+	public void setDepartment(String department)
 	{
-		return courseDescription;
-	}
-
-	/**
-	 * Sets the course description.
-	 *
-	 * @param courseDescription the new course description
-	 */
-	public void setCourseDescription(String courseDescription)
-	{
-		this.courseDescription = courseDescription;
-	}
-
-	/**
-	 * Gets the course number.
-	 *
-	 * @return the course number
-	 */
-	public int getCourseNumber()
-	{
-		return courseNumber;
-	}
-
-	/**
-	 * Sets the course number.
-	 *
-	 * @param courseNumber the new course number
-	 */
-	public void setCourseNumber(int courseNumber)
-	{
-		this.courseNumber = courseNumber;
-	}
-	
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object o)
-	{
-		if(o instanceof Course)
-		{
-			Course other = (Course)o;
-			if(other.courseNumber == courseNumber)
-				return true;
-			else
-				return false;
-		}
-		return false;
-	}
-	
-	/**
-	 * Gets the course request key.
-	 *
-	 * @return the course request key
-	 */
-	public String getCourseRequestKey()
-	{
-		return courseRequestKey;
-	}
-	
-	/**
-	 * Sets the course request key.
-	 *
-	 * @param courseRequestKey the new course request key
-	 */
-	public void setCourseRequestKey(String courseRequestKey)
-	{
-		this.courseRequestKey = courseRequestKey;
+		this.department = department;
 	}
 }
